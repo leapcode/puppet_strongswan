a1e300788f2fe2d4a498e42cdca3a4bd30002bdb
mh <mh@immerda.ch>
2012-12-31 15:40:46 +0100

    also relocate these files

--- manifests/centos/six.pp
+++ manifests/centos/six.pp
@@ -2,6 +2,13 @@ class strongswan::centos::six inherits strongswan::base {
   Service['ipsec']{
     name => 'strongswan',
   }
+  File['/etc/ipsec.secrets']{
+    path => '/etc/strongswan/ipsec.secrets'
+  }
+  File['/etc/ipsec.conf']{
+    path => '/etc/strongswan/ipsec.conf'
+  }
+
   file{'/etc/selinux/strongswan':
     content => "config='/etc/strongswan/strongswan.conf'\n",
     notify  => Service['ipsec'],

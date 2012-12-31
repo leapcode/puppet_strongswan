cc53a58dfa6aef4791bb77b24d40a05f3e621643
mh <mh@immerda.ch>
2012-12-31 16:25:05 +0100

    fix lookup for cert

--- lib/facter/strongswan_cert.rb
+++ lib/facter/strongswan_cert.rb
@@ -1,5 +1,10 @@
 Facter.add("strongswan_cert") do
   setcode do
-      File.exists?( "/etc/ipsec.d/certs/#{Facter.value(:fqdn)}.asc" ) ? File.read( "/etc/ipsec.d/certs/#{Facter.value(:fqdn)}.asc" ) : false
+    cert_path = "certs/#{Facter.value(:fqdn)}.asc"
+    if d = ['/etc/ipsec.d','/etc/strongswan'].find{|d| File.exists?(File.join(d,cert_path)) } 
+      File.read(File.join(d,cert_path))
+    else
+      false
+    end
   end
 end

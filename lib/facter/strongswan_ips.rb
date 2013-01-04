94a86ffc5b8b563f5ba7d95e508a4ea6699f6776
mh <mh@immerda.ch>
2013-01-04 16:00:55 +0100

    ensure that it still works even if the output is nil

--- lib/facter/strongswan_ips.rb
+++ lib/facter/strongswan_ips.rb
@@ -2,7 +2,7 @@ Facter.add("strongswan_ips") do
   setcode do
     result = nil
     if bin = ['/usr/sbin/ipsec', '/usr/sbin/strongswan'].find{|f| File.exists?(f) } 
-      output = Facter::Util::Resolution.exec("#{bin} statusall | grep -E '^  [0-9a-f]'").split("\n").collect(&:strip)
+      output = Facter::Util::Resolution.exec("#{bin} statusall | grep -E '^  [0-9a-f]'").to_s.split("\n").collect(&:strip)
       result = output.join(',') unless output.empty?
     end
     result

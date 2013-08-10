12b26d899b700be9735903fd457d54f75b0638aa
o <o@immerda.ch>
2013-08-10 19:10:46 +0200

    fix syntax

--- lib/facter/strongswan_ips.rb
+++ lib/facter/strongswan_ips.rb
@@ -6,7 +6,7 @@ Facter.add("strongswan_ips") do
     return unless bin
     output = Facter::Util::Resolution.exec(
         "#{bin} statusall | grep -E '^  [0-9a-f]' | sort | uniq")
-            .to_s.split("\n").collect(&:strip)
+    output = output.to_s.split("\n").collect(&:strip)
     output.join(',') unless output.empty?
   end
 end
